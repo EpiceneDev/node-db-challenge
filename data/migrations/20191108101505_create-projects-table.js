98cb617baee36ac1445131f427bfe463d8ae73f7
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-11-08 10:36:14 -0800

    migration tables added

--- data/migrations/20191108101505_create-projects-table.js
+++ data/migrations/20191108101505_create-projects-table.js
@@ -3,16 +3,18 @@ exports.up = function(knex) {
     return knex.schema
         .createTable('project', tbl => {
             tbl.increments();
+            
             tbl.string("name").notNullable().unique();
+
             tbl.text('desc', 256);
-            tbl.boolean('completed');
+
+            tbl.boolean('completed').defaultTo(false);
+
             tbl.integer('resource_id')
                 .unsigned()
                 .references("resources.id")
                 .onDelete("RESTRICT")
                 .onUpdate("CASCADE");
-
-                
         })
 };
 
---
+++ data/migrations/20191108102426_create-projects-tasks-table.js
@@ -0,0 +1,30 @@
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('projects-tasks', tbl => {
+            tbl.increments();
+
+            tbl
+                .integer("project_id")
+                .unsigned()
+                .notNullable()
+                .references("projects.id")
+                .onDelete("CASCADE")
+                .onUpdate("CASCADE");
+
+            tbl
+                .integer("task_id")
+                .unsigned()
+                .notNullable()
+                .references("tasks.id")
+                .onDelete("CASCADE")
+                .onUpdate("CASCADE");
+
+            tbl 
+                .boolean("complete").defaultTo(false)
+        })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+      .dropTableIfExists('projects-task');
+  };
